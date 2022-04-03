165f98237e02ecbcb0b1c8f83da78351b2dcf1dd
8igmac <8igmac@pm.me>
2022-04-03 16:01:15 +0800

    [FEAT] Subscribe and register as driver.

--- server.js
+++ server.js
@@ -8,11 +8,77 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/status', (req, res) => res.json({clients: clients.length}));
+const subscribe = (req, res, isDriver) => {
+    const headers = {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache',
+    };
+    res.writeHead(200, headers);
 
-app.post('/api/schedule', (req, res) => {
-    // TODO: Regester clients as driver.
-    console.log('Driver post schedules.');
+    const clientId = Date.now();
+
+    const newClient = {
+        id: clientId,
+        isDriver,
+        res
+    };
+
+    clients.push(newClient);
+    // '\n' indicates end of event.
+    res.write(`${JSON.stringify({id: clientId})}\n`);
+
+    req.on('close', () => {
+        console.log(`${clientId} Connection closed.`);
+        clients = clients.filter(client => client.id !== clientId);
+    });
+    return newClient;
+};
+
+
+// Usage:
+//
+// const data = {
+//     type: 'driverFinished',
+//     content: 'driver finished',
+// };
+// boradcast(JSON.stringify(data));//debug
+const boradcast = (data) => {
+    clients.forEach(client =>client.res.write(`${data}\n`));
+}
+
+const sendMessage = (data, id) => {
+    const target = clients.find((client) => client.id === id);
+    target.res.write(`${data}\n`);
+}
+
+app.get('/status', (req, res) => {
+    res.json({
+        driver: clients.filter((client) => client.isDriver).length,
+        passenger: clients.filter((client) => !client.isDriver).length,
+    });
+});
+
+app.get('/api/subscribe', (req, res) => {
+    console.log('Client subscribed');
+    const client = subscribe(req, res, false);
+    const data = {
+        type: 'gift',
+        content: 'Subscribed to server.'
+    }
+    sendMessage(JSON.stringify(data), client.id);
+});
+
+app.get('/api/register_driver/:id', (req, res) => {
+    // Register client as driver.
+    let client = clients.find((client) => client.id === parseInt(req.params.id));
+    if (client) {
+        console.log(`Register client ${client.id} as driver.`);
+        client.isDriver = true;
+        res.json({status: `Register ${client.id} as a driver.`});
+    } else {
+        console.log('Client not found.');//debug
+    }
 });
 
 app.get('/api/request_drivers', (req, res) => {
@@ -21,10 +87,31 @@ app.get('/api/request_drivers', (req, res) => {
 });
 
 app.post('/api/choose_driver', (req, res) => {
-    // TODO: 
-    // - Pick the first driver from the clients and create a match.
-    // - Broadcast the match to driver and passenger.
-    console.log('Choose driver.');
+    // Resgister the client as passenger.
+    const passenger = subscribe(req, res, false);
+
+    // Pick the first driver from the clients. 
+    const driver = clients.find((client) => client.isDriver).id;
+
+    // Create a match.
+    const match = {
+        driverId: driver.id,
+        passengerId: passenger.id,
+    }
+    matches.push(match);
+    console.log(`match created, passenger id: ${passenger.id}, driver id: ${driver.id}`);//debug
+
+    // Broadcast the match to driver and passenger.
+    const passengerData = {
+        type: 'driverMatched',
+        content: 'We matched a driver for you.',
+    };
+    sendMessage(JSON.stringify(passengerData), passenger.id);
+    const driverData = {
+        type: 'passengerMatched',
+        content: 'A passenger needs a ride.',
+    }
+    sendMessage(JSON.stringify(driverData), driver.id);
 }); 
 
 app.post('/api/driver_location', (req, res) => {
